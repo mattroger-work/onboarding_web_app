d62c2882aca466c2eabc508372637a331e702525
Matt Roger <mattroger.work@gmail.com>
2020-06-16 08:59:02 -0400

    commented out attachments and every function but sendmail

--- controllers/mail_controller.js
+++ controllers/mail_controller.js
@@ -101,7 +101,25 @@ exports.send_mail_sub = async function(client, per_email, first_name, last_name,
               }
             ],
             //change must also add to the b64 file
-            "attachments": [
+            "attachments": [/*
+              {
+                "@odata.type": "#microsoft.graph.fileAttachment",
+                name: "teksynap-banner-logo.jpg",
+                contentType: "image/jpeg",
+                contentBytes: att.image
+              },
+              {
+                "@odata.type": "#microsoft.graph.fileAttachment",
+                name: "BMS 7.8.0 Email Signature In Outlook WebApp.pdf",
+                contentType: "application/pdf",
+                contentBytes: att.pdf
+              },
+              {
+                "@odata.type": "#microsoft.graph.fileAttachment",
+                name: "Change LinkedIn Background.docx",
+                contentType: "application/msword",
+                contentBytes: att.docx
+              },*/
               {
                 "@odata.type": "#microsoft.graph.fileAttachment",
                 name: "Accessing Microsoft Partner University.pdf",
